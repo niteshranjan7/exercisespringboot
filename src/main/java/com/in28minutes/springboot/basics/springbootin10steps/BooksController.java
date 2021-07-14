e708bc4d0c93e039fb97fd8170e056511b27daef
nitesh <nit.it.esh@gmail.com>
2021-07-14 09:54:51 +0530

    “check”

---
+++ src/main/java/com/in28minutes/springboot/basics/springbootin10steps/BooksController.java
@@ -0,0 +1,23 @@
+package com.in28minutes.springboot.basics.springbootin10steps;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class BooksController {
+
+    @GetMapping("/books")
+    public List<Book> getAllBooks()
+    {
+        return Arrays.asList(new Book(1l, "Mastering in SpringBoot", "Ranga"));
+    }
+
+/*
+legasy -> aws
+    lift and shift
+    validation/security/authToken/Payment/Logging/
+*/
+}
